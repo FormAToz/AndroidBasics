656780e2e581b763a330b69ba9489bfa1c0cb2d5
a.danilov <a.danilov@platformaofd.ru>
2022-03-03 21:05:30 +0300

    Working with getting strings and colors from xml-res file. Also setting resources from code

--- app/src/main/java/com/example/androidfirstproject/MainActivity.java
+++ app/src/main/java/com/example/androidfirstproject/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.androidfirstproject;
 
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,6 +11,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.myscreen);
+        setContentView(R.layout.main);
+
+        final LinearLayout llBottom = findViewById(R.id.llBottom);
+        llBottom.setBackgroundResource(R.color.llBottomColor);
+
+        final TextView tView = findViewById(R.id.tvBottom);
+        tView.setText(R.string.tvTopText);
+
+        final Button btn = findViewById(R.id.btnBottom);
+        btn.setText(R.string.btnBottomText);
     }
 }
